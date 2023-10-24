2faca9f8053acd5e35ef374b0aab5c2f8fcaaec4
Nitin <nm9812546243@gmail.com>
2023-10-24 21:51:01 +0530

    finalized for hosting

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const connectDb = require("./config/mongoose");
 const mongoose = require("mongoose");
 const multer = require("multer");
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const absolutePath = path.join(__dirname, "uploads");
+    return cb(null, absolutePath);
+  },
+  filename: async function (req, file, cb) {
+    const filename = `${Date.now()}-${file.originalname}`;
+    return cb(null, filename);
+  },
+});
+const upload = multer({ storage: storage });
 const app = express();
 require("dotenv").config();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 app.use(express.urlencoded());
 
 // using routes
+app.post("/profile", upload.single("avatar"), function (req, res, next) {
+  console.log(req.file);
+  res.send();
+});
 app.use("/", require("./routes/router"));
 
 connectDb();
